5d84f06105f55f941a183f590071f6f9be20f66b
Camila <camiluc.peli@gmail.com>
2022-01-18 17:45:45 +0100

    2nd challenge

--- OmniChallenges/Challenges.cs
+++ OmniChallenges/Challenges.cs
@@ -12,14 +12,43 @@ namespace OmniChallenges
 
         }
 
-        public static void FoamLetters()
+        public static int HowManyBillboards(string letters)
         {
+            var omnidrone = "omnidre";
 
+            var ammountOfLetters = omnidrone.Select(character =>
+            {
+                var count = character == 'o' || character == 'n'
+                    ? letters.Count(l => l == character) / 2
+                    : letters.Count(l => l == character);
+                return (character, count);
+            });
+
+            return ammountOfLetters.Min(min => min.count);
         }
 
-        public static void FallingBalls()
+        public static int HowManyTimes(double h, double f, double m)
         {
+            // if factor is 1, height won't change, so it will bounce infinitely
+            // also if the factor is higher than 1, it means the ball is gaining energy everytime it hits the floor,
+            // so, even if m is higher than the initial h, eventually it will reach m and it will continue reaching it infinitely
+            if (f >= 1) return -1;
 
+            var count = 0;
+            // we want to count how many time the ball bounces until it doesn't reach the "m" height anymore
+            while (h >= m)
+            {
+                // down
+                count++;
+
+                // hits the floor
+                h *= f;
+
+                // goes up and reaches the "m" height
+                if (h >= m) count++;
+            }
+           
+            return count;
         }
 
         public static void HalfSplits()
--- OmniChallengesTests/ChallengesTests.cs
+++ OmniChallengesTests/ChallengesTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using OmniChallenges;
 
 namespace OmniChallengesTests
 {
@@ -16,15 +17,23 @@ namespace OmniChallengesTests
         }
 
         [Test]
-        public void FoamLettersTest()
+        [TestCase("omnidrone", 1)] // We did cut all the required letters to make 1 billboard.
+        [TestCase("mnidrone", 0)] // This time we're missing one letter, so not even a single billboard can be made.
+        [TestCase("omnidroneomnidrone", 2)] // We cut twice as much as needed.
+        [TestCase("oommnniiddrroonnee", 2)] // They are arranged differently, but there are still enough letters to build 2 billboards.
+        [TestCase("droneomni", 1)] // Order doesn't matter.
+        [TestCase("dowrgmyrwnwretidergrogewrnecdovsdmewfwngregritrewdtwrbfsonvdser", 2)]
+        public void TestFoamLetters(string input, int expected)
         {
-            Assert.Pass();
+            Assert.AreEqual(expected, Challenges.HowManyBillboards(input));
         }
 
         [Test]
-        public void FallingBallTest()
+        [TestCase(3, 0.66, 1.5, 3)] // The ball will pass during first fall, then will bounce passing one time going upwards and one going downwards.
+        [TestCase(3, 1, 1.5, -1)]   // This time the ball bounces infinitely
+        public void TestFallingBall(double h, double f, double m, int expected)
         {
-            Assert.Pass();
+            Assert.AreEqual(expected, Challenges.HowManyTimes(h, f, m));
         }
 
         [Test]
