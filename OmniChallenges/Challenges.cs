62048fe6dc725adebafd3fccc73f85097b59c3f3
Camila <camiluc.peli@gmail.com>
2022-01-18 16:55:09 +0100

    Add project files.

---
+++ OmniChallenges/Challenges.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OmniChallenges
+{
+    public class Challenges
+    {
+        public static void Calculator()
+        {
+
+        }
+
+        public static void FoamLetters()
+        {
+
+        }
+
+        public static void FallingBalls()
+        {
+
+        }
+
+        public static void HalfSplits()
+        {
+
+        }
+
+        public static void ThemePark()
+        {
+
+        }
+    }
+}
---
+++ OmniChallengesTests/ChallengesTests.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+
+namespace OmniChallengesTests
+{
+    public class ChallengesTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CalculatorTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void FoamLettersTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void FallingBallTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void HalfSplitsTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ThemeParkTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
